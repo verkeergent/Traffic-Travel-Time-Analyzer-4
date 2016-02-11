106a2d02655e12b3ca3e843c65b92fa18ec7ae9f
dkkerkho <Dwight.Kerkhove@UGent.be>
2016-02-11 13:23:01 +0100

    [Issue #2] Update & insert toegevoegd

--- applicatie/VerkeerDAL/src/main/java/be/ugent/verkeer4/verkeerdal/DbSet.java
+++ applicatie/VerkeerDAL/src/main/java/be/ugent/verkeer4/verkeerdal/DbSet.java
@@ -1,6 +1,6 @@
 package be.ugent.verkeer4.verkeerdal;
 
-
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -9,25 +9,73 @@ import org.sql2o.Sql2o;
 
 public class DbSet<T> {
 
-    private Sql2o sql2o;
-    private Class<T> type;
+    private final Sql2o sql2o;
+    private final Class<T> type;
+
+    private final String insertQuery;
+    private final String updateQuery;
 
     public DbSet(Class<T> type, Sql2o sql2o) {
         this.type = type;
         this.sql2o = sql2o;
+
+        this.insertQuery = buildInsertQuery();
+        this.updateQuery = buildUpdateQuery();
+    }
+
+    private String buildInsertQuery() {
+        StringBuilder names = new StringBuilder();
+        StringBuilder values = new StringBuilder();
+        for (Field field : this.type.getDeclaredFields()) {
+            if (!field.getName().equalsIgnoreCase(getPrimaryKey())) {
+                names.append(field.getName())
+                        .append(",");
+                values.append(":")
+                        .append(field.getName())
+                        .append(",");
+            }
+        }
+        return "INSERT INTO " + getTableName() + " (" + names.toString().substring(0, names.length() - 1) + ")"
+                + " VALUES (" + values.toString().substring(0, values.length() - 1) + ")";
+    }
+
+    private String buildUpdateQuery() {
+
+        StringBuilder values = new StringBuilder();
+        for (Field field : this.type.getDeclaredFields()) {
+            if (!field.getName().equalsIgnoreCase(getPrimaryKey())) {
+
+                values.append(field.getName())
+                        .append("=")
+                        .append(":").append(field.getName())
+                        .append(",");
+            }
+        }
+        return "UPDATE " + getTableName() + " SET " + values.toString().substring(0, values.length() - 1)
+                + " WHERE " + getPrimaryKey() + "=:" + getPrimaryKey();
     }
 
     public List<T> getItems() {
         try (org.sql2o.Connection con = sql2o.open()) {
-            List<T> lst = con.createQuery("SELECT * from " + this.type.getSimpleName())
+            List<T> lst = con.createQuery("SELECT * from " + getTableName())
                     .executeAndFetch(this.type);
             return lst;
         }
     }
 
+    public T getItem(int key) {
+        try (org.sql2o.Connection con = sql2o.open()) {
+            Query q = con.createQuery("SELECT * from " + getTableName() + " WHERE " + getPrimaryKey() + "= :" + getPrimaryKey())
+                    .addParameter(getPrimaryKey(), key);
+
+            T obj = q.executeAndFetchFirst(this.type);
+            return obj;
+        }
+    }
+
     public T getItem(String condition, Map<String, Object> parameters) {
         try (org.sql2o.Connection con = sql2o.open()) {
-            Query q = con.createQuery("SELECT * from " + this.type.getSimpleName() + " WHERE " + condition);
+            Query q = con.createQuery("SELECT * from " + getTableName() + " WHERE " + condition);
 
             for (Entry<String, Object> parameter : parameters.entrySet()) {
                 q.addParameter(parameter.getKey(), parameter.getValue());
@@ -37,4 +85,41 @@ public class DbSet<T> {
         }
     }
 
+    public int insert(T object) {
+        try (org.sql2o.Connection con = sql2o.open()) {
+
+            Query q = con.createQuery(insertQuery)
+                    .bind(object);
+            Object key = q.executeUpdate().getKey();
+            return (int) (long) key;
+        }
+    }
+
+    public void update(T object) throws Exception {
+        try (org.sql2o.Connection con = sql2o.open()) {
+
+            Query q = con.createQuery(updateQuery)
+                    .bind(object);
+
+            if (getPrimaryKey().equalsIgnoreCase("id")) { // sql2o bind methode voegt geen 'id' toe
+                try {
+                    Field f = this.type.getDeclaredField(getPrimaryKey());
+                    f.setAccessible(true);
+                    q.addParameter(getPrimaryKey(), f.get(object));
+                } catch (NoSuchFieldException | SecurityException ex) {
+                    throw new Exception("Primary key field is niet gevonden op het object");
+                }
+            }
+
+            q.executeUpdate();
+        }
+    }
+
+    protected String getPrimaryKey() {
+        return "id";
+    }
+
+    protected String getTableName() {
+        return this.type.getSimpleName();
+    }
 }
--- applicatie/VerkeerDAL/src/test/java/be/ugent/verkeer4/verkeerdal/test/DbAccessTest.java
+++ applicatie/VerkeerDAL/src/test/java/be/ugent/verkeer4/verkeerdal/test/DbAccessTest.java
@@ -3,33 +3,35 @@ package be.ugent.verkeer4.verkeerdal.test;
 import be.ugent.verkeer4.verkeerdal.Repository;
 import be.ugent.verkeer4.verkeerdomain.data.Traject;
 import java.util.List;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import junit.framework.TestCase;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 
-public class DbAccessTest  extends TestCase {
-    
-    
+public class DbAccessTest extends TestCase {
+
     private Repository repo;
-    
+
     public DbAccessTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() throws ClassNotFoundException {
         repo = new Repository();
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -41,8 +43,48 @@ public class DbAccessTest  extends TestCase {
         } catch (Exception e) {
             fail("Error: " + e.getMessage());
         }
-        
-        
     }
-  
+
+    public void testGetSingleTraject() {
+        try {
+            Traject traject = repo.getTrajectSet().getItem(1);
+            assertNotNull("Traject niet gevonden", traject);
+        } catch (Exception e) {
+            fail("Error: " + e.getMessage());
+        }
+    }
+
+    public void testInsertTraject() {
+        Traject t = new Traject();
+        t.setNaam("Test traject");
+        t.setAfstand(1234);
+
+        int id = repo.getTrajectSet().insert(t);
+        assertNotSame("Id is 0", 0, id);
+
+        t.setId(id);
+
+        Traject storedTraject = repo.getTrajectSet().getItem(id);
+
+        assertEquals("Naam is niet hetzelfde", t.getNaam(), storedTraject.getNaam());
+        assertEquals("Afstand is niet hetzelfde", t.getAfstand(), storedTraject.getAfstand());
+    }
+
+    public void testUpdateTraject() {
+
+        Traject t = repo.getTrajectSet().getItem(1);
+        t.setId(0);
+        int id = repo.getTrajectSet().insert(t); // copy
+        t.setId(id);
+        t.setNaam("Updated naam" + t.getNaam());
+        try {
+            repo.getTrajectSet().update(t);
+        } catch (Exception ex) {
+            fail("Error: " + ex.getMessage());
+        }
+
+        Traject storedTraject = repo.getTrajectSet().getItem(id);
+        assertEquals("Naam is niet hetzelfde", t.getNaam(), storedTraject.getNaam());
+    }
+
 }
