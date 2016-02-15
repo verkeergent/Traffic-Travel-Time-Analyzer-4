ca4e159bd10da7496ac467f3b7afce0fca73014d
Aaron Mousavi <Aaron.Mousavi@UGent.be>
2016-02-15 21:03:36 +0100

    [Issue #26] Finished config file feature

--- applicatie/VerkeerDomain/src/main/java/be/ugent/verkeer4/verkeerdomain/Settings.java
+++ applicatie/VerkeerDomain/src/main/java/be/ugent/verkeer4/verkeerdomain/Settings.java
@@ -3,35 +3,46 @@ package be.ugent.verkeer4.verkeerdomain;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 
-
-// TODO wat propere settings voorzien, eens onderzoeken hoe dit in java best moet
 public class Settings {
-    
+
+    private static final String DB_DEV_CONFIG = "database-dev.conf";
+    private static final String DB_PROD_CONFIG = "database-prod.conf";
+    private static final String SETTINGS_CONFIG = "application.conf";
+
     private static Settings instance;
+
     public static Settings getInstance() {
-        if(instance == null)
+        if (instance == null) {
             instance = new Settings();
+        }
         return instance;
     }
-    
+
     public String getTomTomRoutingAPIKey() {
-        return "5j7n539vbsbf6frb7kwzxtc6";
+        Config conf = ConfigFactory.load(SETTINGS_CONFIG);
+        return conf.getString("APIKeys.TomTom");
     }
-    
-    
-    public  String getDatabaseConnectionString() {
-        return "//localhost:3306/verkeer";
+
+    public String getDatabaseConnectionString() {
+        String configName = inProduction() ? DB_PROD_CONFIG : DB_DEV_CONFIG;
+        Config conf = ConfigFactory.load(configName);
+        return conf.getString("database.connection");
     }
-    
+
     public String getDatabaseUser() {
-        Config conf = ConfigFactory.load();
-        String user = conf.getString("database.user");
-        return user;
+        String configName = inProduction() ? DB_PROD_CONFIG : DB_DEV_CONFIG;
+        Config conf = ConfigFactory.load(configName);
+        return conf.getString("database.user");
     }
-    
+
     public String getDatabasePassword() {
-        Config conf = ConfigFactory.load();
-        String pw = conf.getString("database.password");
-        return pw;
+        String configName = inProduction() ? DB_PROD_CONFIG : DB_DEV_CONFIG;
+        Config conf = ConfigFactory.load(configName);
+        return conf.getString("database.password");
+    }
+
+    public boolean inProduction() {
+        Config conf = ConfigFactory.load(SETTINGS_CONFIG);
+        return conf.getBoolean("general.inProduction");
     }
 }
