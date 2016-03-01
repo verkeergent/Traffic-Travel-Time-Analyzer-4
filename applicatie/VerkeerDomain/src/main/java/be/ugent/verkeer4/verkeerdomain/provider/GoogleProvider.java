4874028a14aa95b48293475dabb95c468d0b59f4
nieverbe <nieverbe.Verbeeck@UGent.be>
2016-03-01 09:47:21 +0100

    Google Service verwijderen omdat deze geen traffic data terug geeft. Overgestapt op JSON

--- applicatie/VerkeerDomain/src/main/java/be/ugent/verkeer4/verkeerdomain/provider/GoogleProvider.java
+++ applicatie/VerkeerDomain/src/main/java/be/ugent/verkeer4/verkeerdomain/provider/GoogleProvider.java
@@ -5,28 +5,16 @@
  */
 package be.ugent.verkeer4.verkeerdomain.provider;
 
-import be.ugent.verkeer4.verkeerdomain.Settings;
 import be.ugent.verkeer4.verkeerdomain.data.ProviderEnum;
 import be.ugent.verkeer4.verkeerdomain.data.Route;
 import be.ugent.verkeer4.verkeerdomain.data.RouteData;
 import be.ugent.verkeer4.verkeerdomain.provider.google.Element;
 import be.ugent.verkeer4.verkeerdomain.provider.google.GoogleMapsClient;
 import be.ugent.verkeer4.verkeerdomain.provider.google.Row;
-import be.ugent.verkeer4.verkeerdomain.provider.here.HereMapsClient;
-import be.ugent.verkeer4.verkeerdomain.provider.here.Response;
-import com.google.maps.DistanceMatrixApi;
-import com.google.maps.GeoApiContext;
-import com.google.maps.model.DistanceMatrix;
-import com.google.maps.model.DistanceMatrixElementStatus;
-import com.google.maps.model.LatLng;
 import java.io.IOException;
-import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.joda.time.DateTime;
-import org.joda.time.MutableDateTime;
-import org.joda.time.ReadWritableDateTime;
-import org.joda.time.ReadableInstant;
+
 
 /**
  *
@@ -34,23 +22,12 @@ import org.joda.time.ReadableInstant;
  */
 public class GoogleProvider extends BaseProvider implements IProvider {
 
-    String appCode = Settings.getInstance().getGoogleRoutingAPPCode();
-    GeoApiContext context = new GeoApiContext().setApiKey(appCode);
-    DistanceMatrix matrix;
-
-
     public GoogleProvider() {
         super(ProviderEnum.GoogleMaps);
     }
    
     @Override
     public RouteData poll(Route route) {
-        //return viaLibrary(route);
-        return viaJson(route);      
-    }   
-    
-    private RouteData viaJson(Route route)
-    {
         try {
             // haal route gegevens op
             Row row = GoogleMapsClient.GetRoute(route.getFromLatitude(), route.getFromLongitude(), route.getToLatitude(), route.getToLongitude());
@@ -71,40 +48,7 @@ public class GoogleProvider extends BaseProvider implements IProvider {
         } catch (IOException ex) {
             Logger.getLogger(GoogleProvider.class.getName()).log(Level.SEVERE, null, ex);
             return null;
-        }
-    }
+        }     
+    }   
     
-    public RouteData viaLibrary(Route route)
-    {
-        try {
-            LatLng origin = new LatLng(route.getFromLatitude(),route.getFromLongitude());
-            LatLng destination = new LatLng(route.getToLatitude(),route.getToLongitude());
-            
-            // haal route gegevens op
-            matrix = DistanceMatrixApi.newRequest(context).origins(origin).destinations(destination).departureTime(new DateTime()).await();  
-            
-            
-            
-            if (matrix.rows.length > 0) {
-             
-                if(matrix.rows[0].elements[0].status == DistanceMatrixElementStatus.OK)
-                {
-                    RouteData rd = setRouteData(route,
-                            (int)matrix.rows[0].elements[0].distance.inMeters,
-                            (int)matrix.rows[0].elements[0].duration.inSeconds,
-                            0);     //Not implemented
-
-                    return rd;
-                }
-                else
-                    return null;
-            }
-
-            return null;
-        } catch (Exception ex) {
-            Logger.getLogger(GoogleProvider.class.getName()).log(Level.SEVERE, null, ex);
-            
-            return null;
-        }    
-    }
 }
