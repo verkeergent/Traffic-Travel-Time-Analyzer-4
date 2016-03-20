cd10c9e86fac9bf00f3dd113458e1c3f79e06540
nieverbe <nieverbe.Verbeeck@UGent.be>
2016-03-20 18:47:50 +0100

    WeatherAPI branch met Develop gemerged!

--- applicatie/VerkeerPollService/src/main/java/be/ugent/verkeer4/verkeerpollservice/Main.java
+++ applicatie/VerkeerPollService/src/main/java/be/ugent/verkeer4/verkeerpollservice/Main.java
@@ -1,24 +1,9 @@
 package be.ugent.verkeer4.verkeerpollservice;
 
-import be.ugent.verkeer4.verkeerdomain.IPOIService;
-import be.ugent.verkeer4.verkeerdomain.IProviderService;
-import be.ugent.verkeer4.verkeerdomain.IRouteService;
-import be.ugent.verkeer4.verkeerdomain.IWeatherService;
-import be.ugent.verkeer4.verkeerdomain.POIService;
-import be.ugent.verkeer4.verkeerdomain.ProviderService;
-import be.ugent.verkeer4.verkeerdomain.RouteService;
-import be.ugent.verkeer4.verkeerdomain.WeatherService;
-import be.ugent.verkeer4.verkeerdomain.data.BoundingBox;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Main {
-
-    private static final int EVERY_MILLIS = 300000;
-    private static List<String> stations = new ArrayList<>();
 
     public static void main(String[] args) throws ClassNotFoundException {
 
