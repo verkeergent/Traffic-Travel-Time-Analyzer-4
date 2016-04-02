42b27cd5ab043d7deef5d1f018b3d884525ba650
dkkerkho <Dwight.Kerkhove@UGent.be>
2016-04-02 17:22:17 +0200

    Documentatie scrapers voor route gegevens

--- applicatie/Scrapers/coyote.pl
+++ applicatie/Scrapers/coyote.pl
@@ -1,40 +1,51 @@
-
+############################
+# Coyote.pl scraped de coyote website waar alle routes worden opgelijst.
+# De naam van de route zal later gematched worden met de naam in de database
+############################
 
 use strict;
+
+# Maak gebruik van de XS::JSON library
 use JSON;
 
+# Coyote heeft een username & wachtwoord nodig om in te loggen
 my $username = "110971610";
 my $password = "50c20b94";
 
-
+# redirect output naar /dev/null of NUL naargelang linux of windows
 my $redir = "/dev/null";
 if($^O eq 'MSWin32') {
 	$redir = "NUL";
 }
+
+# Logt in op coyote zodat er een PHPSESSID cookie wordt teruggegeven
+# De curl statement komt van Chrome -> Developer Tools -> Copy url as curl command line
+# Zorg ook dat er redirects gefollowed worden met --max-redirs
 my $execHeader = "curl --insecure -s -D- \"https://maps.coyotesystems.com/traffic/index.php\" -H \"Origin: https://maps.coyotesystems.com\" -H \"Accept-Encoding: gzip, deflate\" -H \"Accept-Language: en-US,en;q=0.8\" -H \"User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2453.0 Safari/537.36\" -H \"HTTPS: 1\" -H \"Content-Type: application/x-www-form-urlencoded\" -H \"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\" -H \"Cache-Control: max-age=0\" -H \"Referer: https://maps.coyotesystems.com/traffic/login.php\" -H \"Connection: keep-alive\" --data \"login=$username&password=$password\" -L --location-trusted --max-redirs 10 -o $redir";
 
-#print $execHeader;
+# De curl statement geeft enkel de headers terug aangezien we enkel geinteresseerd zijn in de Set-Cookie, vb  Set-Cookie: PHPSESSID=1nsrjm6n9tacpv2us4394ba552; path=/
 my $headers = `$execHeader`;
 
-
-# Set-Cookie: PHPSESSID=1nsrjm6n9tacpv2us4394ba552; path=/
-
+# Parse de PHPSESSID uit de headers met regex
 if($headers =~ /.*PHPSESSID=(.*);/g) {
 	
-	#print "Session found, is $1\n";
+	# de session id is de 1e capture groep
 	my $session = $1;
-	
+	# de time wordt gebruikt zodat de gegevens niet gecached worden (de ?_=... query parameter)
 	my $time = time;
-
+	# vraag de json gegevens op op dezelfde manier als op de website
 	my $exec = "curl --insecure -s -o - \"https://maps.coyotesystems.com/traffic/ajax/get_perturbation_list.ajax.php?_=$time\" -H \"Cookie: PHPSESSID=$session; _ga=GA1.2.1517436056.1456162908\" -H \"Accept-Language: en-US,en;q=0.8\" -H \"User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2453.0 Safari/537.36\" -H \"Accept: application/json, text/javascript, */*; q=0.01\" -H \"Referer: https://maps.coyotesystems.com/traffic/index.php\" -H \"X-Requested-With: XMLHttpRequest\" -H \"Connection: keep-alive\" -L --location-trusted  --max-redirs 10";
 	
+	# het resultaat is een json
 	my $json =  `$exec`;
 	
-	
+	# parse de json naar een object tree met XS:Json
 	my $response =  from_json($json);	
 
+	# print de header in ; formaat
 	print "totalDistanceMeters;totalTimeSeconds;totalDelaySeconds;name\n";
 	
+	# overloop het resultaat, Coyote heeft nog nooit van arrays gehoord en gebruikt overal aparte objecten voor
 	for my $rootElement (keys %{ $response }) {
 		if($rootElement ne "alerts") {
 			for my $routeName (keys %{ $response->{$rootElement} }) {
@@ -45,10 +56,12 @@ if($headers =~ /.*PHPSESSID=(.*);/g) {
 				my $normaltime = $route->{"normal_time"};
 				my $realtime = $route->{"real_time"};
 				
+				# bepaal vertraging door het verschil te nemen van de real time met de normale tijd
 				my $delay = $realtime - $normaltime;
 				if($delay < 0) {
 					$delay = 0;
 				}
+				# print de verzamelde gegevens in een ; formaat
 				print $distance . ";" . int($realtime) . ";" . int($delay) . ";" . $routeName . "\n";
 			}
 		}
--- applicatie/Scrapers/viamichelin.pl
+++ applicatie/Scrapers/viamichelin.pl
@@ -1,21 +1,30 @@
+############################
+# ViaMichelin.pl scraped de route details met gegeven  van - tot geocoordinaten
+############################
 use strict;
 
+# Check of alle argumenten zijn opgegeven
 if(scalar @ARGV < 4) {
-	print "Usage: tomtom fromlat fromlng tolat tolng\n";
+	print "Usage: viamichelin fromlat fromlng tolat tolng\n";
 	exit(1);
 }
 
+# Argumenten 
 my $fromLat = @ARGV[0]; #"51.05633";
 my $fromLng = @ARGV[1]; #"3.69485";
 my $toLat = @ARGV[2]; #"51.038768";
 my $toLng = @ARGV[3]; #"3.736953";
 
+# voer de main functie uit de de gegevens zal opvragen en printen
 main();
 
 sub main {
 
+	# we moeten eerst op zoek gaan naar een geldige API Key die we kunnen gebruiken
+	# de API Key cachen we in een file zodat dit niet steeds opnieuw moet opgevraagd worden
 	my $apiKey;
 	
+	# check of de api gecached is
 	my $cache = readCache();
 	if(exists $cache->{"apikey"} && $cache->{"apikey"} ne "") {
 		$apiKey = $cache->{"apikey"};
@@ -26,16 +35,18 @@ sub main {
 		saveAPIKey($apiKey);
 	}
 	
-	#print $apiKey;
-	#print "\n";
+	# haal de route gegevens op
 	printRouteData($apiKey);
-
 }
 
+##################
+# Leest de gecachede properties van de cache file
+##################
 sub readCache {
 	my @args = @ARGV;
 	@ARGV = ( "viamichelin.cache" );
 	
+	# check de last modified time van de  cache file
 	(my $dev,my $ino,my $mode,my $nlink,my $uid,my $gid,my $rdev,my $size,
    my $atime,my $mtime,my $ctime,my $blksize,my $blocks) =  stat("viamichelin.cache");
    
@@ -46,18 +57,22 @@ sub readCache {
 		return {};
    }		
 	
+	# lees alle lijnen van de vorm key=value en steek ze in een hash
 	my $obj = {};
-	
 	while(<>) {
 		chomp;
 		my @parts = split("=", $_);
 		$obj->{$parts[0]} = $parts[1];
 	}
 	
+	# restore de oorpsronkelijke args en return de hash
 	@ARGV = @args;
 	return $obj;
 }
 
+#################
+# Saved de API key in de cache
+#################
 sub saveAPIKey {
 	(my $apikey) = @_;
 	
@@ -66,18 +81,17 @@ sub saveAPIKey {
 	close $fh;
 }
 
-
+########################
+# Haalt de API Key op uit de website html
+########################
 sub getAPIKey {
 	my $url = "http://www.viamichelin.com/web/Routes";
 	my $response = `curl -s -o - '$url'`;
 	
-	
-	# eerst substringen naar de apikey: is STUKKEN sneller
-	#print $response;
+	# eerst substringen naar de apikey: is STUKKEN sneller	
 	my $idx = index($response, "/js?key=");
 	my $part = substr($response,$idx, 200);
 	
-	#print $part;
 	if($part =~ /.*=(.*)\$/gc) {
 		return $1;
 	}
@@ -86,43 +100,49 @@ sub getAPIKey {
 	exit(1);
 }
 
-
+##############################
+# Print de route data met behulp van de gegeven api key
+##############################
 sub printRouteData {
 	(my $apiKey) = @_;
 	
+	# bouw url op om de route jsonp response terug te krijgen
 	my $url = 'http://vmrest.viamichelin.com/apir/4/iti.json/eng/geom;header;roadsheet?steps=1:e:' . $fromLng . ':' . $fromLat . ';1:e:' . $toLng . ':' . $toLat . ';&distUnit=m&veht=0&itit=1&avoidClosedRoad=false&currency=EUR&fuelCost=1.36&fuelConsump=6.8:5.6:5.6&favMotorways=false&avoidBorders=false&avoidTolls=false&avoidCCZ=false&avoidORC=false&avoidPass=false&wCaravan=false&withSecurityAdv=true&fullMapOpt=300:300:true:true:true&stepMapOpt=300:300:true:true:true&multipleIti=true&traffic=AUT;BEL;CHE;DEU;DNK;ESP;FIN;FRA;GBR;ITA;LUX;NLD;NOR;POL;SWE&obfuscation=false&ie=UTF-8&charset=UTF-8&callback=JSE.HTTP.asyncRequests[32].HTTPResponseLoaded&authKey=' . $apiKey . '&lg=eng&nocache=' . time;
 		
-	#print $url;
-	#print "$url\n";
+	# vraag gegevens op met curl en store de jsonp response
 	my $response = `curl --insecure -s --globoff -o - "$url"`;
 	
+	# subtstring het stuk dat we willen hebben
 	my $idx = index($response, "totalDist");
 	my $part = substr($response,$idx, 1500);
-	
+
+	# parameters die er uit moeten gehaald worden
 	my $trafficTime;
 	my $baseTime;
 	my $distance;
 	
-	#print $response;
-	#print $part;
-	
+	# Parse met regex de trafficTime:value json property
 	if($part =~ /.*?\"?trafficTime\"?\:\s*(.*?),.*?/) {
 		$trafficTime = int($1);
 	}
 	
+	# Parse met regex de drivingTime:value json property
 	if($part =~ /.*?\"?drivingTime\"?\:\s*(.*?),.*?/) {
 		$baseTime = int($1);
 	}
 	
+	# Parse met regex de drivingDist:value json property
 	if($part =~ /.*?\"?drivingDist\"?\:\s*(.*?),.*?/) {
 		$distance = int($1);
 	}
 	
+	# bereken vertraging
 	my $delay = $trafficTime - $baseTime;
 	if($delay < 0) {
 		$delay = 0;
 	}
 	
+	# print header en data
 	print "totalDistanceMeters;totalTimeSeconds;totalDelaySeconds\n";
 	print $distance . ";" . $trafficTime . ";" . $delay . "\n";
 }
